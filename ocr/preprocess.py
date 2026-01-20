e277007e8ce48afe24aafc1ca888f8487d38046d
gauravn17 <93863977+gauravn17@users.noreply.github.com>
2026-01-19 20:07:28 -0800

    Update preprocess.py

--- ocr/preprocess.py
+++ ocr/preprocess.py
@@ -1,5 +1,10 @@
 # ocr/preprocess.py
+import sys
+from pathlib import Path
 
+ROOT_DIR = Path(__file__).resolve().parents[1]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
 import cv2
 import numpy as np
 
